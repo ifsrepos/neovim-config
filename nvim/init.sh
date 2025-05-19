910c7fff63d507c249c9b0365aa213f31df604b8
Iván Fernández Samaniego <ivan@sesa.es>
2025-05-19 08:49:02 +0200

    Modified home reference

--- nvim/init.sh
+++ nvim/init.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-NORD_DIR="~/.local/share/nvim/site/pack/packer/start/nord.nvim"
-PACKER_DIR="~/.local/share/nvim/site/pack/packer/start/packer.nvim"
-TREESITTER_DIR="~/.local/share/nvim/site/pack/packer/start/nvim-treesitter.nvim"
-LSP_DIR="~/.local/share/nvim/site/pack/"
+NORD_DIR="${HOME}/.local/share/nvim/site/pack/packer/start/nord.nvim"
+PACKER_DIR="${HOME}/.local/share/nvim/site/pack/packer/start/packer.nvim"
+TREESITTER_DIR="${HOME}/.local/share/nvim/site/pack/packer/start/nvim-treesitter.nvim"
+LSP_DIR="${HOME}/.local/share/nvim/site/pack/"
 
 if [ ! -d "$PACKER_DIR" ]; then
 	mkdir -p "$PACKER_DIR"
@@ -21,13 +21,13 @@ if [ ! -d "$TREESITTER_DIR" ]; then
 fi
 
 # Install Lua Language Server
-rm -rf ~/lua-language-server
+rm -rf ${HOME}/lua-language-server
 sudo apt install ninja-build -y
-git clone https://github.com/LuaLS/lua-language-server ~/lua-language-server
-cd ~/lua/bash-language-server
+git clone https://github.com/LuaLS/lua-language-server ${HOME}/lua-language-server
+cd ${HOME}/lua/bash-language-server
 ./make.sh
-echo '[INFO] Add ~/lua-language-server/bin/ to the PATH in ~/.bashrc' 
-rm -rf ~/lua-language-server
+echo '[INFO] Add ${HOME}/lua-language-server/bin/ to the PATH in ${HOME}/.bashrc' 
+rm -rf ${HOME}/lua-language-server
 
 # Install others language servers
 npm install -g pyright
